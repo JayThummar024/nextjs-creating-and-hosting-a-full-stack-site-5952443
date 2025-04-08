9f4cf27eaf975ff28e0ddfd066e1a59454649e36
JayThummar024 <74243199+JayThummar024@users.noreply.github.com>
2025-04-08 20:12:17 +0000

    Initial commit

--- ecommerce/app/cart/page.tsx
+++ ecommerce/app/cart/page.tsx
@@ -1,14 +1,5 @@
-import ShoppingCartList from "./ShoppingCartList";
-
-export const dynamic = 'force-dynamic';
-
-export default async function CartPage() {
-  const response = await fetch(process.env.NEXT_PUBLIC_SITE_URL + '/api/users/2/cart', {
-    cache: 'no-cache',
-  });
-  const cartProducts = await response.json();
-
+export default function CartPage() {
   return (
-    <ShoppingCartList initialCartProducts={cartProducts} />
+    <div>Cart</div>
   );
 }
